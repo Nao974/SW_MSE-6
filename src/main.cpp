0718d05c62b68797f860041519c3f3ee3e5f27d6
Nao974 <Nao974@gmail.com>
2023-09-14 11:39:00 +0400

    add recalculate motor range

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 BluetoothSerial SerialBT;
 //                      Pilotage,  Trim,     Joystick X, Joystick Y                        
 uint8_t btReceive[4] = {0,         0,        0,         0};
-/*
+
 #include <L298N.h>
 Moteur moteurAvGauche, moteurAvDroite, moteurArGauche, moteurArDroite;
 const uint16_t vitesse = 255;
-*/
+
 // Formated data
 int16_t stickX= 0, stickY=0;
 
@@ -18,11 +18,11 @@ void setup() {
     SerialBT.begin("MSE-6");
 
 // Initialization of motors
-/*	moteurAvGauche.init(2,   4,  3); // IN1, IN2, ENA
+	moteurAvGauche.init(2,   4,  3); // IN1, IN2, ENA
 	moteurAvDroite.init(5,   7,  6); // IN1, IN2, ENA
 	moteurArGauche.init(8,   9, 10); // IN1, IN2, ENA
 	moteurArDroite.init(12, 13, 11); // IN1, IN2, ENA    
-*/
+
 // Initialization of Serial debug
     Serial.begin(9600);
 }
@@ -36,10 +36,10 @@ void loop() {
         btReceive[3] = SerialBT.read(); // Joystick Y
         Serial.printf("Pilotage= %d // Trim= %d // Joy X= %d // Joy Y=%d\n", btReceive[0], btReceive[1], btReceive[2], btReceive[3]);
 
-/*        // Recalculates data in servo range
-        stickX = map(btReceive[1],0, 248, -225, 225 );
-        stickY = map(btReceive[2],0, 248, 110, -110 );
-        //Serial.printf("Direction= %d // Stick X= %d // Stick Y=%d\n", btReceive[0], stickX, stickY); */
+// Recalculates data in motor range
+        stickX = map(btReceive[2],0, 250, -255, 255 )+btReceive[1];
+        stickY = map(btReceive[3],0, 250, 255, -255 )+btReceive[1];
+        Serial.printf("Stick X= %d // Stick Y=%d\n", btReceive[0], stickX, stickY);
     }
 
 /*
