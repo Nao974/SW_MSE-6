a84e4570a70780c732fa06d3bed95e8feea64ff8
Nao-Pop <nao-tumu@hotmail.fr>
2023-09-16 12:47:28 +0400

    test deadline

--- include/L298N.h
+++ include/L298N.h
@@ -6,13 +6,14 @@ struct Moteur {
 	uint8_t pinENA;
 	uint8_t deadZone;
 
-	void init( uint8_t _pinIN1, uint8_t _pinIN2, uint8_t _pinENA) {
+	void init( uint8_t _pinIN1, uint8_t _pinIN2, uint8_t _pinENA, uint8_t _deadZone) {
 		pinIN1 = _pinIN1;
 		pinMode(pinIN1, OUTPUT);
 		pinIN2 = _pinIN2;
 		pinMode(pinIN2, OUTPUT);
 		pinENA = _pinENA;
-		pinMode(pinENA, OUTPUT);	
+		pinMode(pinENA, OUTPUT);
+		deadZone = _deadZone;
 	}
 
 	void speed(int16_t vitesse) {
@@ -25,6 +26,7 @@ struct Moteur {
 			digitalWrite(pinIN2, HIGH);	
 			vitesse = - vitesse;	
 		}
+//		vitesse = map(vitesse, 0,255, deadZone, 255);
 		analogWrite(pinENA, vitesse);
 		}				
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,31 @@ uint8_t btReceive[4] = {0,         0,        0,         0};
 
 #include <L298N.h>
 Moteur moteurAvGauche, moteurAvDroite, moteurArGauche, moteurArDroite;
-
-// Formated data
 int16_t vitesseBase=0, directionBase=0, vitesseGauche= 0, vitesseDroite=0;
 
+#define DEBUG	false
+
 void setup() {
+// Initialization of Serial debug
+    #if DEBUG== true 
+		Serial.begin(115200);
+	#endif
+
 // Initilization of the Bluetooth server
-	Serial.println("SerialBT init");
+	#if DEBUG== true 
+		Serial.println("SerialBT init"); 
+	#endif
     SerialBT.begin("MSE-6");
 
 // Initialization of motors
-	moteurAvGauche.init(18,  5, 19); // IN1, IN2, ENA
-	moteurAvDroite.init(17, 16,  4); // IN1, IN2, ENA
-	moteurArDroite.init(14, 27, 12); // IN1, IN2, ENA
-	moteurArGauche.init(26, 25, 33); // IN1, IN2, ENA
+	#if DEBUG== true 
+		Serial.println("Motors init"); 
+	#endif
+	moteurAvGauche.init(18,  5, 19, 70); // IN1, IN2, ENA
+	moteurAvDroite.init(17, 16,  4, 140); // IN1, IN2, ENA
+	moteurArGauche.init(26, 25, 33, 100); // IN1, IN2, ENA
+	moteurArDroite.init(14, 27, 12, 105); // IN1, IN2, ENA
 
-// Initialization of Serial debug
-    Serial.begin(115200);
 }
 
 void loop() {
@@ -33,12 +41,10 @@ void loop() {
         btReceive[1] = SerialBT.read(); // Trim
         btReceive[2] = SerialBT.read(); // Joystick X Direction
         btReceive[3] = SerialBT.read(); // Joystick Y Vitesse
-        Serial.printf("Pilotage= %d // Trim= %d // Joy X= %d // Joy Y=%d\n", btReceive[0], btReceive[1], btReceive[2], btReceive[3]);
 
 		// Recalculates data in motor range
         directionBase = map(btReceive[2],0, 250, -255, 255 )+btReceive[1];
         vitesseBase = map(btReceive[3],0, 250, 255, -255 );
-		 //Serial.printf("Vitesse base= %d",vitesseBase);
 
 		vitesseGauche= vitesseBase + directionBase;
 		vitesseDroite= vitesseBase - directionBase;
@@ -48,14 +54,16 @@ void loop() {
 		if ( vitesseDroite > 255 ) vitesseDroite = 255;
 		if ( vitesseDroite < -255 ) vitesseDroite = -255;
 
+		#if DEBUG == true
+			Serial.printf("Pilotage= %d // Trim= %d // Joy X= %d // Joy Y=%d\n", btReceive[0], btReceive[1], btReceive[2], btReceive[3]);
+			Serial.printf("Vitesse base= %d, direction base=%d",vitesseBase,directionBase);
+			Serial.printf("Moteurs Gauche= %d // Moteurs Droit=%d\n", vitesseGauche, vitesseDroite);
+		#endif
+
 		moteurAvGauche.speed(vitesseGauche);
 		moteurAvDroite.speed(vitesseDroite);
 		moteurArGauche.speed(vitesseGauche);
 		moteurArDroite.speed(vitesseDroite);
-       // Serial.printf("Moteurs Gauche= %d // Moteurs Droit=%d\n", vitesseGauche, vitesseDroite);
     }
-
-
-
 
 }
