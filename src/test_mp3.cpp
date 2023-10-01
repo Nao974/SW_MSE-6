091b5e24f4efc4ee792863b6db478fd8a954b9c4
Nao-Pop <nao-tumu@hotmail.fr>
2023-10-01 18:53:05 +0400

    final test MP3 player OK

--- src/test_mp3.cpp
+++ src/test_mp3.cpp
@@ -3,48 +3,37 @@
 
 // Alternatively initialise on another serial port.
 // https://circuits4you.com/2018/12/31/esp32-hardware-serial2-example/
-// https://github.com/SnijderC/dyplayer
 const uint8_t MP3RX2 = 22;
 const uint8_t MP3TX2 = 23;
+
+// YPlayer Arduino: https://github.com/SnijderC/dyplayer
 DY::Player player(&Serial2);
 
+#define DEBUG   true
 
 void setup() {
+    // Init serial for comannd MP3 Player
     Serial2.begin(9600, SERIAL_8N1, MP3RX2, MP3TX2);
 
-  player.begin();
-  // Also initiate the hardware serial port so we can use it for debug printing
-  // to the console..
-  Serial.begin(9600);
-  player.setVolume(15); 
-  // player.setCycleMode(DY::PlayMode::Sequence); // Play all and stop.
-  player.setCycleMode(DY::PlayMode::Repeat); // Play all and repeat.
-  //player.setCycleMode(DY::PlayMode::Random); // Play all randomly and repeat.
-    //player.next();
- //player.previous (); 
- //player.volumeIncrease();
-  //player.volumeDecrease();
-  
- //player.playSpecified(4);
+    // Init Player
+    player.begin();
+
+    #if DEBUG == true
+        Serial.begin(115200);
+    #endif
+
+  player.setVolume(25); //Set the playback volume between 0 and 30. Default volume if not set: 20
+  //player.setCycleMode(DY::PlayMode::Repeat); // Play all and repeat.
+  player.setCycleMode(DY::PlayMode::Random); // Play all randomly and repeat.
   player.play();
 }
 
 void loop() {
 
-  // Print the number of the sound that is playing.
-  Serial.print("Playing sound: ");
-  Serial.println((int16_t)player.getPlayingSound());
-  delay(3000);
-    
+    #if DEBUG== true
+        Serial.print("Playing sound: ");
+        Serial.println((int16_t)player.getPlayingSound());
+        delay(2000);
+    #endif    
   
 }
-/*
-  // For some reason periods need to be stars for this module.
-  // Future releases will do this automatically.
- /* char path[] = "/00001.MP3";
-  player.playSpecifiedDevicePath(DY::Device::Flash, path);
-  delay(500);
-  Serial.print("Playing sound: ");
-  Serial.println((int16_t)player.getPlayingSound());
-  delay(4000);
-*/
